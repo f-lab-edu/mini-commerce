b85f800ebb11226cea1fad4c5d1e24a7f4c06c7b
bg-yu <byunggun.yu@gmail.com>
2023-04-04 22:20:20 +0900

    tab -> space 4칸으로 변경

--- src/main/java/com/flab/commerce/web/response/Response.java
+++ src/main/java/com/flab/commerce/web/response/Response.java
@@ -13,33 +13,33 @@ import org.springframework.http.HttpStatus;
 @Builder(access = AccessLevel.PRIVATE)
 public class Response {
 
-	private HttpStatus status;
+    private HttpStatus status;
 
-	private Integer code;
+    private Integer code;
 
-	private Object data;
+    private Object data;
 
-	private String error;
+    private String error;
 
-	public static Response of() {
-		return Response.builder()
-				.status(HttpStatus.OK)
-				.code(HttpStatus.OK.value())
-				.data("")
-				.build();
-	}
+    public static Response of() {
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .code(HttpStatus.OK.value())
+                .data("")
+                .build();
+    }
 
-	public static Response of(HttpStatus status) {
-		return Response.builder()
-				.status(status)
-				.code(status.value())
-				.build();
-	}
+    public static Response of(HttpStatus status) {
+        return Response.builder()
+                .status(status)
+                .code(status.value())
+                .build();
+    }
 
-	public Response addData(Object data) {
-		this.data = data;
-		return Response.builder()
-				.data(this.data)
-				.build();
-	}
+    public Response addData(Object data) {
+        this.data = data;
+        return Response.builder()
+                .data(this.data)
+                .build();
+    }
 }
